847690eec54b46cbc2ff378bdab2a87793c20984
yntlokwana <yntlokwana@gmail.com>
2022-11-20 15:26:55 +0200

    fixing extent report test class on testng.xml

--- src/main/java/PageObjects/SignInPage.java
+++ src/main/java/PageObjects/SignInPage.java
@@ -44,7 +44,7 @@ public class SignInPage extends BaseClass {
     }
      public boolean isUserSignedIn()
      {
-         action.explicitWait(getDriver(),signOutBtn,30);
+            action.explicitWait(getDriver(),signOutBtn,40);
             return action.isDisplayed(getDriver(),signOutBtn);
      }
 }
